26c3ef42789d5a2ff32c669e7dbed8e092b30dcf
Ayotunde <ayopedro2012@gmail.com>
2023-06-12 21:50:49 +0100

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,9 @@ import * as requestIp from 'request-ip';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
